cb1efb03c32e376c928a2024fa3e59999401e80c
Artur Efimov <arturefimov@gmail.com>
2020-06-08 18:51:56 +0300

    g001

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# goAndOberonOpenGL
-Go and Oberon examples of OpenGL 3D graphics
+# Go Oberon OpenGL
+
+Go and (Oberon)[https://freeoberon.su/en] examples of OpenGL 3D graphics.
+
+Примеры программ, использующих 3D-графику с использованием OpenGL на языках Go и (Oberon)[https://freeoberon.su].
+
+## How To Use
+1. Install Go, OpenGL and SDL2.
+```
+go get -u github.com/go-gl/gl/v3.3-core/gl
+go get -u github.com/veandco/go-sdl2/sdl
+```
+
+2. Clone this repository:
+```
+git clone git@github.com:kekcleader/goAndOberonOpenGL.git
+```
+or download it.
+
+3. Go to subdirectory `g001\_simplest` and type:
+```
+make
+```
+to compile and run the program.
+
+## Source Files
+
+| File Name | Description |
+| [g001\_simplest](g001_simplest/g001_simplest.go) | Simplest Go Example of 3D graphics using OpenGL + SDL2. Draws a triangle on the screen. |
---
+++ g001_simplest/g001_simplest.go
@@ -0,0 +1,162 @@
+// g001_simplest.go
+// Simplest Go Example of 3D graphics using OpenGL + SDL2.
+// Draws a triangle on the screen.
+
+package main
+
+import (
+  "github.com/veandco/go-sdl2/sdl"
+  "github.com/go-gl/gl/v3.3-core/gl"
+  "fmt"
+  "io/ioutil"
+  "strings"
+)
+
+const (
+  sW = 1280
+  sH = 720
+)
+
+var (
+  shaderProg uint32
+  vao uint32
+  vbo uint32
+  win *sdl.Window
+)
+
+func readShader(fname string, s *string) {
+  b, err := ioutil.ReadFile(fname)
+  if err != nil {
+    fmt.Println("Could not read file '" + fname + "'.")
+    panic(err)
+  }
+  *s = string(b) + "\x00"
+}
+
+func loadShader(fname string, shader *uint32, sType uint32) {
+  sh := gl.CreateShader(sType)
+
+  var src0 string
+  readShader(fname, &src0)
+
+  src, free := gl.Strs(src0)
+  gl.ShaderSource(sh, 1, src, nil)
+  free()
+
+  gl.CompileShader(sh)
+
+  var status int32
+  gl.GetShaderiv(sh, gl.COMPILE_STATUS, &status)
+  if status == gl.FALSE {
+    var logLen int32
+    gl.GetShaderiv(sh, gl.INFO_LOG_LENGTH, &logLen)
+    log := strings.Repeat("\x00", int(logLen + 1))
+    gl.GetShaderInfoLog(sh, logLen, nil, gl.Str(log))
+    panic("Failed to compile shader '" + fname + "':\n" + log)
+  }
+
+  *shader = sh;
+}
+
+func initShaderProgram(shaderProg *uint32) {
+  var vShader, fShader uint32
+  loadShader("shaders/vertex.txt", &vShader, gl.VERTEX_SHADER)
+  loadShader("shaders/fragment.txt", &fShader, gl.FRAGMENT_SHADER)
+
+  pr := gl.CreateProgram()
+  gl.AttachShader(pr, vShader)
+  gl.AttachShader(pr, fShader)
+  gl.LinkProgram(pr)
+
+  var status int32
+  gl.GetProgramiv(pr, gl.LINK_STATUS, &status)
+  if status == gl.FALSE {
+    var logLen int32
+    gl.GetProgramiv(pr, gl.INFO_LOG_LENGTH, &logLen)
+    log := strings.Repeat("\x00", int(logLen + 1))
+    gl.GetProgramInfoLog(pr, logLen, nil, gl.Str(log))
+    panic("Failed to link shader program\n" + log)
+  }
+
+  gl.DeleteShader(vShader)
+  gl.DeleteShader(fShader)
+
+  *shaderProg = pr
+}
+
+func init1() {
+  var err error
+  err = sdl.Init(sdl.INIT_EVERYTHING)
+  if err != nil { panic(err) }
+
+  win, err = sdl.CreateWindow("a1", 200, 100, sW, sH, sdl.WINDOW_OPENGL)
+  //if err != nil { panic(err) }
+  win.GLCreateContext()
+
+  gl.Init()
+  version := gl.GoStr(gl.GetString(gl.VERSION))
+  fmt.Println("OpenGL version", version)
+
+  initShaderProgram(&shaderProg)
+
+  v := []float32{
+    -0.5, -0.5, 0,
+    0.5, -0.5, 0,
+    0, 0.5, 0,
+  }
+
+  gl.GenBuffers(1, &vbo)
+  gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+
+  gl.GenVertexArrays(1, &vao)
+  gl.BindVertexArray(vao)
+
+  gl.BufferData(gl.ARRAY_BUFFER, len(v) * 4, gl.Ptr(v), gl.STATIC_DRAW)
+  gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3 * 4, nil)
+  gl.EnableVertexAttribArray(0)
+  gl.BindVertexArray(0)
+}
+
+func draw() {
+  gl.ClearColor(0, 0, 0, 0)
+  gl.Clear(gl.COLOR_BUFFER_BIT)
+  gl.UseProgram(shaderProg)
+  gl.BindVertexArray(vao)
+  gl.DrawArrays(gl.TRIANGLES, 0, 3)
+  win.GLSwap()
+}
+
+func HandleKey(e *sdl.KeyboardEvent, done *bool) {
+  if e.Keysym.Sym == sdl.K_ESCAPE {
+    *done = true
+  }
+}
+
+func run() {
+  var event sdl.Event
+  var done bool
+
+  done = false
+  for !done {
+    event = sdl.PollEvent()
+    for event != nil {
+      switch e := event.(type) {
+        case *sdl.QuitEvent: done = true
+        case *sdl.KeyboardEvent: HandleKey(e, &done)
+      }
+      event = sdl.PollEvent()
+    }
+    draw()
+  }
+}
+
+func done() {
+  win.Destroy()
+  sdl.Quit()
+}
+
+func main() {
+  init1()
+  run()
+  done()
+}
